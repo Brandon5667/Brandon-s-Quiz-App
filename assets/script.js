b0df19a5098d778b6e559f83c0a9eafaff51c5a6
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-02-20 23:11:25 -0500

    fixed form and leaderboard

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,13 @@ var timerEl = document.getElementById('timer');
 var scoreEl = document.getElementById('score');
 var score = 0;
 var availableQuestions = 0;
-var highScore = [];
+var leaderBoard = [];
+var leaderBoardStorage = localStorage.getItem('leaderBoard');
+  if (leaderBoardStorage) {
+      leaderBoard = JSON.parse(leaderBoardStorage);
+  }
+  updateLeaderBoard();
+  
 // array of objects for questions
 var questions = [
   {
@@ -39,65 +45,56 @@ var questions = [
   },
 ];
 
-
+var interval;
 var timer = 100;
 // function to start timer
 function startQuiz() {
   timer = 100
   availableQuestions = 0
-  setInterval(function () {
-      timer--;
-      timerEl.textContent = timer + " time left"
-      
-      if (timer <= 0) {
-          // stop quiz
-          timer = 0
-          artHead.innerHTML = 'Quiz over!!';
-          artBody.innerHTML = 'Here is the highlights from your quiz!';
-          var form = document.createElement('form')
-          form.setAttribute("type", "text")
-          form.setAttribute("placeholder", "Initials")
-          artBody.appendChild(form);
-          var option1 = document.createElement("button")
-    artBody.appendChild(option1);
-    option1.innerHTML = "reset"
-    option1.addEventListener("click", function(){startQuiz(), getQuestion(0)});
-          
-      }
+    interval = setInterval(function () {
+    
+    timer--;
+    timerEl.textContent = timer + " time left"
+
+    if (timer <= 0) {
+      // stop quiz
+      stopQuiz()
+
+    }
   }, 1000);
 
-            artHead.innerHTML = 'Good luck!';
-            artBody.innerHTML = ''; // clear whatever is there currently
-            
-            var ul = document.createElement('ul');
+  artHead.innerHTML = 'Good luck!';
+  artBody.innerHTML = ''; // clear whatever is there currently
+
+  
 }
 // pull a new question and set of answers
 function getQuestion(i) {
-  
-    artBody.innerHTML = questions[i].question;
-    var option1 = document.createElement("button")
-    artBody.appendChild(option1);
-    option1.innerHTML = questions[i].answer[0]
-    option1.addEventListener("click", function(){checker(questions[i].answer[0], questions[i].correctAnswer)});
-    var option2 = document.createElement("button")
-    artBody.appendChild(option2)
-    option2.innerHTML = questions[i].answer[1]
-    option2.addEventListener("click", function(){checker(questions[i].answer[1], questions[i].correctAnswer)});
-    var option3 = document.createElement("button")
-    artBody.appendChild(option3)
-    option3.innerHTML = questions[i].answer[2]
-    option3.addEventListener("click", function(){checker(questions[i].answer[2], questions[i].correctAnswer)});
-    var option4 = document.createElement("button")
-    artBody.appendChild(option4)
-    option4.innerHTML = questions[i].answer[3]
-    option4.addEventListener("click", function(){checker(questions[i].answer[3], questions[i].correctAnswer)});
-              
-              
+
+  artBody.innerHTML = questions[i].question;
+  var option1 = document.createElement("button")
+  artBody.appendChild(option1);
+  option1.innerHTML = questions[i].answer[0]
+  option1.addEventListener("click", function () { checker(questions[i].answer[0], questions[i].correctAnswer) });
+  var option2 = document.createElement("button")
+  artBody.appendChild(option2)
+  option2.innerHTML = questions[i].answer[1]
+  option2.addEventListener("click", function () { checker(questions[i].answer[1], questions[i].correctAnswer) });
+  var option3 = document.createElement("button")
+  artBody.appendChild(option3)
+  option3.innerHTML = questions[i].answer[2]
+  option3.addEventListener("click", function () { checker(questions[i].answer[2], questions[i].correctAnswer) });
+  var option4 = document.createElement("button")
+  artBody.appendChild(option4)
+  option4.innerHTML = questions[i].answer[3]
+  option4.addEventListener("click", function () { checker(questions[i].answer[3], questions[i].correctAnswer) });
+
+
 }
 // check if answer is correct and update score and timer accordingly
-function checker(x, y){
-  if (availableQuestions <4) {
-    if (x===y){
+function checker(x, y) {
+  if (availableQuestions < 4) {
+    if (x === y) {
       score++
       console.log("correct")
       availableQuestions++
@@ -105,15 +102,81 @@ function checker(x, y){
       console.log(score)
       getQuestion(availableQuestions)
     }
-    else{
-      timer=timer-10
+    else {
+      timer = timer - 10
       console.log("incorrect")
       availableQuestions++
       getQuestion(availableQuestions)
-      
+
     }
+  }
+  else {
+    stopQuiz();
+  }
 }
-else{
-  timer = 0
+
+function submitHighScore (initials){
+  leaderBoard.push({ initials, score });
+  localStorage.setItem('leaderBoard', JSON.stringify(leaderBoard));
+  updateLeaderBoard();
 }
+
+function stopQuiz (){
+  clearInterval(interval);
+      timer = 0
+      artHead.innerHTML = 'Quiz over!!';
+      artBody.innerHTML = 'Here is the highlights from your quiz!';
+      var form = document.createElement('form')
+      var input = document.createElement('input')
+      input.setAttribute("type", "text")
+      input.setAttribute("placeholder", "Initials")
+      form.appendChild(input)
+      var submit = document.createElement("button");
+      submit.setAttribute('type', 'button');
+      submit.innerHTML = "Submit";
+      submit.addEventListener('click', function () {submitHighScore(input.value)});
+      form.appendChild(submit);
+      artBody.appendChild(form);
+      var option1 = document.createElement("button")
+      artBody.appendChild(option1);
+      option1.innerHTML = "reset"
+      option1.addEventListener("click", function () { startQuiz(), getQuestion(0) });
 }
+
+function updateLeaderBoard(){
+  var lbDiv = document.getElementById('leaderboard');
+  lbDiv.innerHTML = '';
+  var table = document.createElement('table');
+// create header with initials and score columns
+var thead = document.createElement('thead');
+var headerRow = document.createElement('tr');
+var initialHeaderCol = document.createElement('th');
+initialHeaderCol.innerHTML = 'Initials';
+headerRow.appendChild(initialHeaderCol);
+var scoreHeaderCol = document.createElement('th');
+scoreHeaderCol.innerHTML = 'Score';
+headerRow.appendChild(scoreHeaderCol);
+table.appendChild(thead);
+
+// create body for scores
+var tbody = document.createElement('tbody');
+
+
+for (var i = 0; i < leaderBoard.length; i++) {
+    var scoreRow = document.createElement('tr');
+    var initialTd = document.createElement('td');
+    initialTd.innerHTML = leaderBoard[i].initials;
+    scoreRow.appendChild(initialTd);
+    var scoreTd = document.createElement('td');
+    scoreTd.innerHTML = leaderBoard[i].score;
+    scoreRow.appendChild(scoreTd);
+    tbody.appendChild(scoreRow);
+}
+table.appendChild(tbody);
+lbDiv.appendChild(table);
+}
+
+function showLeaderBoard(){
+  var showBoard = document.getElementById('leaderboard');
+  showBoard.style.display = 'block';
+};
